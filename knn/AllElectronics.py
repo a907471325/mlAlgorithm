b83c4dc81f061c83d80218e9d372b339e8581178
宋禹龙 <907471325@qq.com>
2018-01-01 17:03:54 +0800

    change file path

--- knn/AllElectronics.py
+++ knn/AllElectronics.py
@@ -6,7 +6,8 @@ from sklearn import preprocessing
 from sklearn.externals.six import StringIO
 
 # Read in the csv file and put features into list of dict and list of class label
-allElectronicsData = open(r'F:\BaiduNetdiskDownload\代码与素材\代码与素材(1)\01DTree\AllElectronics.csv', 'rt')
+# allElectronicsData = open(r'F:\BaiduNetdiskDownload\代码与素材\代码与素材(1)\01DTree\AllElectronics.csv', 'rt')
+allElectronicsData = open(r'AllElectronics.csv', 'rt')
 # allData = pd.read_csv('AllElectronics.csv')
 # print(allElectronicsData)
 
